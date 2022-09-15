6c4bddd74f5ba3d18a695064e53d9fdf79eeab52
Bernardo Quetglas <bernardo_quetglas@hotmail.com>
2022-09-14 23:16:53 -0300

    rediseño de scrypts

--- js/tienda.js
+++ js/tienda.js
@@ -10,76 +10,94 @@ let productos = [
     {"name": "Salame picado fino", "price": 850, "id":"prod9", img: "../img/chorizos.jpg"}
 ];
 
-const carrito = [];
-const cantidad = [];
-const total = []; 
-const containerProductos = document.querySelector(".products__gallery");
-//let nombreProducto = [];
-let precio = [];
+const containerDiv = document.querySelector(".products__gallery");
+const carritoDiv = document.querySelector("#carrito_tr");
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-function nombreProducto(){
-    for (const key in productos) {
-        if (Object.hasOwnProperty.call(productos, key)) {
-            const element = productos[key];
-            console.log(element);            
-        }        
-    }    
-};
-nombreProducto()
-
-
-
-function pintarProductos(){
+//FUNCION CREAR CARDS DE PRODUCTOS
+function crearCards(){
     productos.forEach(element=>{
-        containerProductos.innerHTML += 
+        containerDiv.innerHTML +=         
         `<li class="cards">
             <img src="${element.img}" alt="">
             <div class="agregarProducto">
-                <h4>${element.name}</h4>
-                <p>$${element.price}</p>        
-                <button class="boton click" id="${element.id}">Agregar</button>
+                <p>${element.name}</p>
+                <p>$${element.price}</p>      
             </div>
+            <button class="boton click" id="btn-agregar${element.id}">Agregar</button>
         </li>`
     })
-    agregarCarrito();
+    agregarFuncionAlBoton();
 }
 
+//FUNCION AGREGAR EVENTO A BOTON 
+function agregarFuncionAlBoton(){
+    productos.forEach(producto=>{
+        document.querySelector(`#btn-agregar${producto.id}`).addEventListener("click",()=>{
+            agregarAlCarrito(producto)
+        })
+    })
+}
 
-
-//FUNCION AGREGAR PRODUCTOS EN CARRITO
-function  agregarCarrito(){
-    document.querySelectorAll(".click").forEach(el => {
-    el.addEventListener("click", e => {    
-    const id = e.target.getAttribute("id");    
-            
-        for(let i=0; i < productos.length; i++){
-            if(productos[i].id == id){
-            carrito.push(productos[i].name);
-            carrito.push(" $" + productos[i].price);
-            pintarCarro();            
-            }
-            };
-        });
+//FUNCION AGREGAR PRODUCTO AL CARRITO
+    function agregarAlCarrito(producto){
+    let existe = carrito.some(prod=>prod.id === producto.id);
+    if(existe===false){
+        producto.cantidad = 1;
+        carrito.push(producto);
     }
-)};
-agregarCarrito();
-
-//FUNCION PINTAR PRODUCTOS EN CARRITO
-function pintarCarro(){
-    let tr = document.createElement("tr");
-    for(let i = 0; i < carrito.length; i++ ){
-        let td = document.createElement("td");        
-        td.innerHTML = carrito[i];
-        tr.appendChild(td);
+    else{
+        let prodFind = carrito.find(prod=> prod.id===producto.id);
+        prodFind.cantidad++;
     }
+    console.log(carrito);
+    pintarCarrito();
+    totalCarrito()
 
-    for (let i = carrito.length; i > 0; i--) {
-        carrito.pop();
+}
+
+//FUNCION PINTAR CARRITO
+function pintarCarrito(){
+    carritoDiv.innerHTML = "";
+    carrito.forEach(prod=>{
+        carritoDiv.innerHTML += 
+        `<li>
+            <p>${prod.name}</p>
+            <p>CANTIDAD: ${prod.cantidad}</p>
+            <p>$${prod.price}</p>
+            <button class="btnCarrito" id="btn-borrar${prod.id}">Borrar</button>
+            <button class="btnCarrito" id="btn-borrarUnoSolo${prod.id}">-</button>
+        </li>`
+    })
+    localStorage.setItem("carrito",JSON.stringify(carrito))
+    borrarProducto()    
+}
+
+//FUNCION BORRAR PRODUCTOS DEL CARRITO
+function borrarProducto(){
+    carrito.forEach(producto=>{
+        document.querySelector(`#btn-borrar${producto.id}`).addEventListener("click",()=>{
+            let indice = carrito.findIndex(element=>element.id===producto.id);
+            carrito.splice(indice,1);
+            pintarCarrito()
+        })
+    })
+}
+
+//FUNCION SUMAR TOTAL CARRITO
+function totalCarrito(){
+   /* for(let i = 0; i < carrito.length; i++ ){
+        const total = Object.values(carrito).reduce((precio, {cantidad, price}) => precio + (cantidad * price),0);
+        console.log(total);
+    }*/
+    
+    if(Object.keys(carrito).length !== 0){
+        const total = Object.values(carrito).reduce((precio, {cantidad, price}) => precio + (cantidad * price),0);
+        console.log(total);
     }
-    carrito_tr.appendChild(tr)
-};
-pintarCarro();
+}
 
+pintarCarrito();
+crearCards();
 
-pintarProductos()
 
